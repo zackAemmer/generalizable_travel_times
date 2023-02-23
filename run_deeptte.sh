4fde7d7daa2a9701dcc740b9c0df780b16333cd5
Zack <zae5op@gmail.com>
2023-02-23 11:16:43 +0100

    Small changes

--- run_deeptte.sh
+++ run_deeptte.sh
@@ -1,28 +1,28 @@
 #!/bin/zsh
 
 # Move to project folder
-cd ~/Desktop/valle
-# cd ~/Skrivebord/valle
+# cd ~/Desktop/valle
+cd ~/Skrivebord/valle
 
 
-## KCM
-# Copy files to Deeptte
-cp -a ./results/3_mo_cross_val/kcm/deeptte_formatted/* ../DeepTTE/data
-cp ../DeepTTE/data/config.json ../DeepTTE/config.json
+# ## KCM
+# # Copy files to Deeptte
+# cp -a ./results/3_mo_cross_val/kcm/deeptte_formatted/* ../DeepTTE/data
+# cp ../DeepTTE/data/config.json ../DeepTTE/config.json
 
-# Run Deeptte
-cd ~/Desktop/DeepTTE
+# # Run Deeptte
+# # cd ~/Desktop/DeepTTE
 # cd ~/Skrivebord/DeepTTE
-rm -rf ./result && mkdir ./result
-rm -rf ./saved_weights && mkdir ./saved_weights
-rm -rf ./logs && mkdir ./logs
-python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
+# rm -rf ./result && mkdir ./result
+# rm -rf ./saved_weights && mkdir ./saved_weights
+# rm -rf ./logs && mkdir ./logs
+# python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
 
-# Copy Deeptte results back to results folder
-cd ~/Desktop/valle
+# # Copy Deeptte results back to results folder
+# # cd ~/Desktop/valle
 # cd ~/Skrivebord/valle
-cp -a ../DeepTTE/result ./results/3_mo_cross_val/kcm/deeptte_results/
-cp -a ../DeepTTE/saved_weights ./results/3_mo_cross_val/kcm/deeptte_results/
+# cp -a ../DeepTTE/result ./results/3_mo_cross_val/kcm/deeptte_results/
+# cp -a ../DeepTTE/saved_weights ./results/3_mo_cross_val/kcm/deeptte_results/
 
 
 ## ATB
@@ -31,15 +31,15 @@ cp -a ./results/3_mo_cross_val/atb/deeptte_formatted/* ../DeepTTE/data
 cp ../DeepTTE/data/config.json ../DeepTTE_Annotated/config.json
 
 # Run Deeptte
-cd ~/Desktop/DeepTTE
-# cd ~/Skrivebord/DeepTTE
+# cd ~/Desktop/DeepTTE
+cd ~/Skrivebord/DeepTTE
 rm -rf ./result && mkdir ./result
 rm -rf ./saved_weights && mkdir ./saved_weights
 rm -rf ./logs && mkdir ./logs
 python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
 
 # Copy Deeptte results back to results folder
-cd ~/Desktop/valle
+# cd ~/Desktop/valle
 cd ~/Skrivebord/valle
 cp -a ../DeepTTE/result ./results/3_mo_cross_val/atb/deeptte_results/
 cp -a ../DeepTTE/saved_weights ./results/3_mo_cross_val/atb/deeptte_results/
