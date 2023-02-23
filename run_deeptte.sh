0412cd613779670df715ad0ad17eb13a88b81f4b
Zack <zae5op@gmail.com>
2023-02-23 10:06:50 +0100

    For running on remote

--- run_deeptte.sh
+++ run_deeptte.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
 # Move to project folder
-cd ~/Desktop/valle
+# cd ~/Desktop/valle
+cd ~/Skrivebord/valle
+
 
 ## KCM
 # Copy files to Deeptte
@@ -9,14 +11,16 @@ cp -a ./results/3_mo_cross_val/kcm/deeptte_formatted/ ../DeepTTE_Annotated/data
 cp ../DeepTTE_Annotated/data/config.json ../DeepTTE_Annotated/config.json
 
 # Run Deeptte
-cd ~/Desktop/DeepTTE_Annotated
+# cd ~/Desktop/DeepTTE_Annotated
+cd ~/Skrivebord/DeepTTE_Annotated
 rm -rf ./result && mkdir ./result
 rm -rf ./saved_weights && mkdir ./saved_weights
 rm -rf ./logs && mkdir ./logs
-python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte.res --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
+python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
 
 # Copy Deeptte results back to results folder
-cd ~/Desktop/valle
+# cd ~/Desktop/valle
+cd ~/Skrivebord/valle
 cp -a ../DeepTTE_Annotated/result ./results/3_mo_cross_val/kcm/deeptte_results/
 cp -a ../DeepTTE_Annotated/saved_weights ./results/3_mo_cross_val/kcm/deeptte_results/
 
@@ -27,13 +31,15 @@ cp -a ./results/3_mo_cross_val/atb/deeptte_formatted/ ../DeepTTE_Annotated/data
 cp ../DeepTTE_Annotated/data/config.json ../DeepTTE_Annotated/config.json
 
 # Run Deeptte
-cd ~/Desktop/DeepTTE_Annotated
+# cd ~/Desktop/DeepTTE_Annotated
+cd ~/Skrivebord/DeepTTE_Annotated
 rm -rf ./result && mkdir ./result
 rm -rf ./saved_weights && mkdir ./saved_weights
 rm -rf ./logs && mkdir ./logs
-python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte.res --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
+python main.py --task train --batch_size 10 --epochs 50 --result_file ./result/deeptte --pooling_method attention --kernel_size 3 --alpha 0.1 --log_file train_log
 
 # Copy Deeptte results back to results folder
 cd ~/Desktop/valle
+cd ~/Skrivebord/valle
 cp -a ../DeepTTE_Annotated/result ./results/3_mo_cross_val/atb/deeptte_results/
 cp -a ../DeepTTE_Annotated/saved_weights ./results/3_mo_cross_val/atb/deeptte_results/
