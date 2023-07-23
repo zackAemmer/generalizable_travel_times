Normalizing labels, fixing some bugs

src/models/deeptte/DeepTTE.py
@@ -49,9 +49,12 @@ class EntireEstimator(nn.Module):
         pred = pred * std + mean
 
         ### MAPE loss
-        loss = torch.abs(pred - label) / label   ### size [batch, 1]
+        loss_fn = nn.HuberLoss()
+        loss = loss_fn(pred, label)
+        # loss = torch.abs(pred - label) / label   ### size [batch, 1]
 
-        return {'label': label, 'pred': pred}, loss.mean()
+        return {'label': label, 'pred': pred}, loss
+        # return {'label': label, 'pred': pred}, loss.mean()
 
 
 class LocalEstimator(nn.Module):
@@ -81,9 +84,12 @@ class LocalEstimator(nn.Module):
         pred = pred * std + mean
 
         ### MAPE loss
-        loss = torch.abs(pred - label) / (label + EPS)   ### size [variable, 1]
-        
-        return loss.mean()
+        loss_fn = nn.HuberLoss()
+        loss = loss_fn(pred, label)
+        # loss = torch.abs(pred - label) / (label + EPS)   ### size [variable, 1]
+
+        return loss
+        # return loss.mean()
 
 
 class Net(pl.LightningModule):
@@ -140,6 +146,17 @@ class Net(pl.LightningModule):
         attr = batch[0]
         traj = batch[1]
         config = self.config
+        # Norm
+        for k in list(attr.keys()):
+            try:
+                attr[k] = (attr[k] - self.config[f"{k}_mean"]) / self.config[f"{k}_std"]
+            except:
+                continue
+        for k in list(traj.keys()):
+            try:
+                traj[k] = (traj[k] - self.config[f"{k}_mean"]) / self.config[f"{k}_std"]
+            except:
+                continue
         entire_out, (local_out, local_length) = self(attr, traj, config)
         pred_dict, entire_loss = self.entire_estimate.eval_on_batch(entire_out, attr['time'], config['time_mean'], config['time_std'])
         # get the mean/std of each local path

src/models/transformer.py
@@ -127,9 +127,9 @@ class TRSF_L(pl.LightningModule):
         out = torch.cat([x_em, x_ct], dim=2)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
         mask = data_utils.create_tensor_mask(x_sl).to(x_sl)
-        out = out.cpu().numpy()
         mask = mask.cpu().numpy()
-        y = y.cpu().numpy()
+        out  = (out.cpu().numpy() * self.config['time_calc_s_std']) + self.config['time_calc_s_mean']
+        y = (y.cpu().numpy() * self.config['time_calc_s_std']) + self.config['time_calc_s_mean']
         out = data_utils.aggregate_tts(out, mask)
         y = data_utils.aggregate_tts(y, mask)
         return (out, y)
@@ -296,9 +296,9 @@ class TRSF_GRID_L(pl.LightningModule):
         out = torch.cat((x_em, x_ct), dim=2)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
         mask = data_utils.create_tensor_mask(x_sl).to(x_sl)
-        out = out.cpu().numpy()
         mask = mask.cpu().numpy()
-        y = y.cpu().numpy()
+        out  = (out.cpu().numpy() * self.config['time_calc_s_std']) + self.config['time_calc_s_mean']
+        y = (y.cpu().numpy() * self.config['time_calc_s_std']) + self.config['time_calc_s_mean']
         out = data_utils.aggregate_tts(out, mask)
         y = data_utils.aggregate_tts(y, mask)
         return (out, y)

src/run_models.py
@@ -37,8 +37,8 @@ def run_models(run_folder, network_folder, **kwargs):
     print(f"RUN MODELS: '{run_folder}'")
     print(f"NETWORK: '{network_folder}'")
 
-    NUM_WORKERS=4
-    PIN_MEMORY=True
+    NUM_WORKERS=0
+    PIN_MEMORY=False
 
     # Create folder structure; delete older results
     base_folder = f"{run_folder}{network_folder}"
@@ -78,7 +78,6 @@ def run_models(run_folder, network_folder, **kwargs):
     if kwargs['is_param_search']:
         hyperparameter_sample_dict = {
             'n_param_samples': 1,
-            # 'batch_size': [64, 128, 256, 512, 1024, 2048],
             'batch_size': [512],
             'hidden_size': [16, 32, 64, 128],
             'num_layers': [2, 3, 4, 5],
@@ -91,31 +90,31 @@ def run_models(run_folder, network_folder, **kwargs):
     else:
         hyperparameter_dict = {
             'FF': {
-                'batch_size': 128,
+                'batch_size': 512,
                 'hidden_size': 32,
-                'num_layers': 2,
-                'dropout_rate': .1
+                'num_layers': 3,
+                'dropout_rate': .2
             },
             'CONV': {
-                'batch_size': 128,
-                'hidden_size': 32,
-                'num_layers': 2,
-                'dropout_rate': .1
+                'batch_size': 512,
+                'hidden_size': 64,
+                'num_layers': 4,
+                'dropout_rate': .4
             },
             'GRU': {
-                'batch_size': 128,
-                'hidden_size': 32,
+                'batch_size': 512,
+                'hidden_size': 16,
                 'num_layers': 2,
-                'dropout_rate': .1
+                'dropout_rate': .4
             },
             'TRSF': {
-                'batch_size': 128,
+                'batch_size': 512,
                 'hidden_size': 32,
-                'num_layers': 2,
-                'dropout_rate': .1
+                'num_layers': 4,
+                'dropout_rate': .4
             },
             'DEEPTTE': {
-                'batch_size': 128
+                'batch_size': 10
             }
         }
 
@@ -175,9 +174,9 @@ def run_models(run_folder, network_folder, **kwargs):
             print(f"Network {network_folder} Fold {fold_num} Model {model.model_name}")
             train_dataset.add_grid_features = model.requires_grid
             test_dataset.add_grid_features = model.requires_grid
-            train_loader = DataLoader(train_dataset, batch_size=model.batch_size, sampler=train_sampler, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY, multiprocessing_context="fork")
-            val_loader = DataLoader(train_dataset, batch_size=model.batch_size, sampler=val_sampler, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY, multiprocessing_context="fork")
-            test_loader = DataLoader(test_dataset, batch_size=model.batch_size, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY, multiprocessing_context="fork")
+            train_loader = DataLoader(train_dataset, batch_size=model.batch_size, sampler=train_sampler, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY)
+            val_loader = DataLoader(train_dataset, batch_size=model.batch_size, sampler=val_sampler, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY)
+            test_loader = DataLoader(test_dataset, batch_size=model.batch_size, collate_fn=model.collate_fn, drop_last=True, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY)
             if not model.is_nn:
                 # Train base model on all fold data
                 model.train(train_loader, config)
@@ -195,7 +194,7 @@ def run_models(run_folder, network_folder, **kwargs):
                     min_epochs=10,
                     accelerator="cpu",
                     logger=CSVLogger(save_dir=f"{run_folder}{network_folder}logs/", name=model.model_name),
-                    callbacks=[EarlyStopping(monitor=f"{model.model_name}_valid_loss", min_delta=1, patience=3)]
+                    callbacks=[EarlyStopping(monitor=f"{model.model_name}_valid_loss", min_delta=.01, patience=3)]
                     # profiler="simple"
                 )
                 trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
@@ -217,56 +216,84 @@ def run_models(run_folder, network_folder, **kwargs):
 if __name__=="__main__":
     torch.set_default_dtype(torch.float)
     torch.set_float32_matmul_precision('medium')
-    pl.seed_everything(43, workers=True)
+    pl.seed_everything(42, workers=True)
 
-    # # DEBUG
+    # DEBUG
+    run_models(
+        run_folder="./results/debug/",
+        network_folder="kcm/",
+        grid_s_size=500,
+        n_folds=2,
+        holdout_routes=[100252,100139,102581,100341,102720],
+        skip_gtfs=False,
+        is_param_search=False
+    )
+    run_models(
+        run_folder="./results/debug/",
+        network_folder="atb/",
+        grid_s_size=500,
+        n_folds=2,
+        holdout_routes=["ATB:Line:2_28","ATB:Line:2_3","ATB:Line:2_9","ATB:Line:2_340","ATB:Line:2_299"],
+        skip_gtfs=False,
+        is_param_search=False
+    )
+    # DEBUG MIXED
+    run_models(
+        run_folder="./results/debug_nosch/",
+        network_folder="kcm_atb/",
+        grid_s_size=500,
+        n_folds=2,
+        holdout_routes=None,
+        skip_gtfs=True,
+        is_param_search=False
+    )
+
+    # # PARAM SEARCH
     # run_models(
-    #     run_folder="./results/debug/",
+    #     run_folder="./results/param_search/",
     #     network_folder="kcm/",
     #     grid_s_size=500,
-    #     n_folds=2,
+    #     n_folds=3,
+    #     holdout_routes=None,
+    #     skip_gtfs=False,
+    #     is_param_search=True
+    # )
+    # run_models(
+    #     run_folder="./results/param_search/",
+    #     network_folder="atb/",
+    #     grid_s_size=500,
+    #     n_folds=3,
+    #     holdout_routes=None,
+    #     skip_gtfs=False,
+    #     is_param_search=True
+    # )
+
+    # # FULL RUN
+    # run_models(
+    #     run_folder="./results/full_run/",
+    #     network_folder="kcm/",
+    #     grid_s_size=500,
+    #     n_folds=5,
     #     holdout_routes=[100252,100139,102581,100341,102720],
     #     skip_gtfs=False,
     #     is_param_search=False
     # )
     # run_models(
-    #     run_folder="./results/debug/",
+    #     run_folder="./results/full_run/",
     #     network_folder="atb/",
     #     grid_s_size=500,
-    #     n_folds=2,
+    #     n_folds=5,
     #     holdout_routes=["ATB:Line:2_28","ATB:Line:2_3","ATB:Line:2_9","ATB:Line:2_340","ATB:Line:2_299"],
     #     skip_gtfs=False,
     #     is_param_search=False
     # )
-    # # DEBUG MIXED
+    # # FULL RUN MIXED
     # run_models(
-    #     run_folder="./results/debug_nosch/",
+    #     run_folder="./results/full_run_nosch/",
     #     network_folder="kcm_atb/",
     #     grid_s_size=500,
-    #     n_folds=2,
+    #     n_folds=5,
     #     holdout_routes=None,
     #     skip_gtfs=True,
     #     is_param_search=False
     # )
-
-    # PARAM SEARCH
-    run_models(
-        run_folder="./results/param_search/",
-        network_folder="kcm/",
-        grid_s_size=500,
-        n_folds=3,
-        holdout_routes=None,
-        skip_gtfs=False,
-        is_param_search=True
-    )
-    run_models(
-        run_folder="./results/param_search/",
-        network_folder="atb/",
-        grid_s_size=500,
-        n_folds=3,
-        holdout_routes=None,
-        skip_gtfs=False,
-        is_param_search=True
-    )
-
-    # FULL RUN

src/run_preparation.py
@@ -26,6 +26,8 @@ def process_data_parallel(date_list, i, n, **kwargs):
     traces = data_utils.calculate_trace_df(traces, kwargs['timezone'][n], kwargs['epsg'][n], kwargs['grid_bounds'][n], kwargs['coord_ref_center'][n], kwargs['data_dropout'])
     if not kwargs['skip_gtfs']:
         traces = data_utils.clean_trace_df_w_timetables(traces, kwargs['gtfs_folder'][n], kwargs['epsg'][n], kwargs['coord_ref_center'][n])
+        if len(traces)==0:
+            print(f"Lost points from {date_list} when trying to match GTFS")
     traces = data_utils.calculate_cumulative_values(traces, kwargs['skip_gtfs'])
     if len(traces)==0:
         print(f"No data remaining after cleaning for dates: {date_list}")
@@ -189,18 +191,149 @@ def prepare_run(overwrite, run_name, network_name, train_dates, test_dates, **kw
 
 if __name__=="__main__":
 
-    # DEBUG
+    # # DEBUG
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="debug",
+    #     network_name=["kcm"],
+    #     train_dates=data_utils.get_date_list("2023_03_15", 3),
+    #     test_dates=data_utils.get_date_list("2023_03_21", 3),
+    #     n_workers=2,
+    #     n_jobs=2,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/kcm_gtfs/"],
+    #     raw_data_folder=["./data/kcm_all_new/"],
+    #     timezone=["America/Los_Angeles"],
+    #     epsg=["32148"],
+    #     grid_bounds=[[369903,37911,409618,87758]],
+    #     coord_ref_center=[[386910,69022]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=False
+    # )
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="debug",
+    #     network_name=["atb"],
+    #     train_dates=data_utils.get_date_list("2023_03_15", 3),
+    #     test_dates=data_utils.get_date_list("2023_03_21", 3),
+    #     n_workers=2,
+    #     n_jobs=2,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/atb_gtfs/"],
+    #     raw_data_folder=["./data/atb_all_new/"],
+    #     timezone=["Europe/Oslo"],
+    #     epsg=["32632"],
+    #     grid_bounds=[[550869,7012847,579944,7039521]],
+    #     coord_ref_center=[[569472,7034350]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=False
+    # )
+    # # DEBUG MIXED
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="debug_nosch",
+    #     network_name=["kcm","atb"],
+    #     train_dates=data_utils.get_date_list("2023_03_15", 3),
+    #     test_dates=data_utils.get_date_list("2023_03_21", 3),
+    #     n_workers=2,
+    #     n_jobs=2,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/kcm_gtfs/","./data/atb_gtfs/"],
+    #     raw_data_folder=["./data/kcm_all_new/","./data/atb_all_new/"],
+    #     timezone=["America/Los_Angeles","Europe/Oslo"],
+    #     epsg=["32148","32632"],
+    #     grid_bounds=[[369903,37911,409618,87758],[550869,7012847,579944,7039521]],
+    #     coord_ref_center=[[386910,69022],[569472,7034350]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id'],['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=True
+    # )
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="debug_nosch",
+    #     network_name=["rut"],
+    #     train_dates=data_utils.get_date_list("2023_03_15", 3),
+    #     test_dates=data_utils.get_date_list("2023_03_21", 3),
+    #     n_workers=2,
+    #     n_jobs=2,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/rut_gtfs/"],
+    #     raw_data_folder=["./data/rut_all_new/"],
+    #     timezone=["Europe/Oslo"],
+    #     epsg=["32632"],
+    #     grid_bounds=[[589080,6631314,604705,6648420]],
+    #     coord_ref_center=[[597427,6642805]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=True
+    # )
+
+    # # PARAM SEARCH
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="param_search",
+    #     network_name=["kcm"],
+    #     train_dates=data_utils.get_date_list("2023_02_15", 30),
+    #     test_dates=data_utils.get_date_list("2023_04_01", 7),
+    #     n_workers=2,
+    #     n_jobs=8,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/kcm_gtfs/"],
+    #     raw_data_folder=["./data/kcm_all_new/"],
+    #     timezone=["America/Los_Angeles"],
+    #     epsg=["32148"],
+    #     grid_bounds=[[369903,37911,409618,87758]],
+    #     coord_ref_center=[[386910,69022]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=False
+    # )
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
+    # prepare_run(
+    #     overwrite=True,
+    #     run_name="param_search",
+    #     network_name=["atb"],
+    #     train_dates=data_utils.get_date_list("2023_02_15", 30),
+    #     test_dates=data_utils.get_date_list("2023_04_01", 7),
+    #     n_workers=2,
+    #     n_jobs=8,
+    #     data_dropout=0.2,
+    #     gtfs_folder=["./data/atb_gtfs/"],
+    #     raw_data_folder=["./data/atb_all_new/"],
+    #     timezone=["Europe/Oslo"],
+    #     epsg=["32632"],
+    #     grid_bounds=[[550869,7012847,579944,7039521]],
+    #     coord_ref_center=[[569472,7034350]],
+    #     given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
+    #     skip_gtfs=False
+    # )
+
+    # FULL RUN
     random.seed(0)
     np.random.seed(0)
     torch.manual_seed(0)
     prepare_run(
         overwrite=True,
-        run_name="debug",
+        run_name="full_run",
         network_name=["kcm"],
-        train_dates=data_utils.get_date_list("2023_03_15", 3),
-        test_dates=data_utils.get_date_list("2023_03_21", 3),
+        train_dates=data_utils.get_date_list("2023_02_15", 30),
+        test_dates=data_utils.get_date_list("2023_04_01", 7),
         n_workers=2,
-        n_jobs=2,
+        n_jobs=8,
         data_dropout=0.2,
         gtfs_folder=["./data/kcm_gtfs/"],
         raw_data_folder=["./data/kcm_all_new/"],
@@ -216,12 +349,12 @@ if __name__=="__main__":
     torch.manual_seed(0)
     prepare_run(
         overwrite=True,
-        run_name="debug",
+        run_name="full_run",
         network_name=["atb"],
-        train_dates=data_utils.get_date_list("2023_03_15", 3),
-        test_dates=data_utils.get_date_list("2023_03_21", 3),
+        train_dates=data_utils.get_date_list("2023_02_15", 30),
+        test_dates=data_utils.get_date_list("2023_04_01", 7),
         n_workers=2,
-        n_jobs=2,
+        n_jobs=8,
         data_dropout=0.2,
         gtfs_folder=["./data/atb_gtfs/"],
         raw_data_folder=["./data/atb_all_new/"],
@@ -232,18 +365,18 @@ if __name__=="__main__":
         given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
         skip_gtfs=False
     )
-    # DEBUG MIXED
+    # FULL RUN MIXED
     random.seed(0)
     np.random.seed(0)
     torch.manual_seed(0)
     prepare_run(
         overwrite=True,
-        run_name="debug_nosch",
+        run_name="full_run_nosch",
         network_name=["kcm","atb"],
-        train_dates=data_utils.get_date_list("2023_03_15", 3),
-        test_dates=data_utils.get_date_list("2023_03_21", 3),
+        train_dates=data_utils.get_date_list("2023_02_15", 30),
+        test_dates=data_utils.get_date_list("2023_04_01", 7),
         n_workers=2,
-        n_jobs=2,
+        n_jobs=8,
         data_dropout=0.2,
         gtfs_folder=["./data/kcm_gtfs/","./data/atb_gtfs/"],
         raw_data_folder=["./data/kcm_all_new/","./data/atb_all_new/"],
@@ -259,12 +392,12 @@ if __name__=="__main__":
     torch.manual_seed(0)
     prepare_run(
         overwrite=True,
-        run_name="debug_nosch",
+        run_name="full_run_nosch",
         network_name=["rut"],
-        train_dates=data_utils.get_date_list("2023_03_15", 3),
-        test_dates=data_utils.get_date_list("2023_03_21", 3),
+        train_dates=data_utils.get_date_list("2023_02_15", 30),
+        test_dates=data_utils.get_date_list("2023_04_01", 7),
         n_workers=2,
-        n_jobs=2,
+        n_jobs=8,
         data_dropout=0.2,
         gtfs_folder=["./data/rut_gtfs/"],
         raw_data_folder=["./data/rut_all_new/"],
@@ -275,50 +408,3 @@ if __name__=="__main__":
         given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
         skip_gtfs=True
     )
-
-    # PARAM SEARCH
-    random.seed(0)
-    np.random.seed(0)
-    torch.manual_seed(0)
-    prepare_run(
-        overwrite=True,
-        run_name="param_search",
-        network_name=["kcm"],
-        train_dates=data_utils.get_date_list("2023_02_15", 30),
-        test_dates=data_utils.get_date_list("2023_04_01", 7),
-        n_workers=2,
-        n_jobs=8,
-        data_dropout=0.2,
-        gtfs_folder=["./data/kcm_gtfs/"],
-        raw_data_folder=["./data/kcm_all_new/"],
-        timezone=["America/Los_Angeles"],
-        epsg=["32148"],
-        grid_bounds=[[369903,37911,409618,87758]],
-        coord_ref_center=[[386910,69022]],
-        given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
-        skip_gtfs=False
-    )
-    random.seed(0)
-    np.random.seed(0)
-    torch.manual_seed(0)
-    prepare_run(
-        overwrite=True,
-        run_name="param_search",
-        network_name=["atb"],
-        train_dates=data_utils.get_date_list("2023_02_15", 30),
-        test_dates=data_utils.get_date_list("2023_04_01", 7),
-        n_workers=2,
-        n_jobs=8,
-        data_dropout=0.2,
-        gtfs_folder=["./data/atb_gtfs/"],
-        raw_data_folder=["./data/atb_all_new/"],
-        timezone=["Europe/Oslo"],
-        epsg=["32632"],
-        grid_bounds=[[550869,7012847,579944,7039521]],
-        coord_ref_center=[[569472,7034350]],
-        given_names=[['trip_id','file','locationtime','lat','lon','vehicle_id']],
-        skip_gtfs=False
-    )
-
-    # # # # FULL RUN
-    

src/utils/data_loader.py
@@ -93,12 +93,16 @@ class LoadSliceDataset(Dataset):
         # Get information on shingle file location and lines; read specific shingle lines from specific shingle file
         samp_dict = self.shingle_lookup[self.shingle_keys[index]]
         samp = self.h5_lookup[f"{self.train_or_test}_data_{samp_dict['network']}_{samp_dict['file_num']}.h5"][samp_dict['start_idx']:samp_dict['end_idx']]
+        label = samp[-1,self.col_names.index("time_cumulative_s")]
+        norm_label = (label - self.config['time_mean']) / self.config['time_std']
+        label_seq = samp[:,self.col_names.index("time_calc_s")]
+        norm_label_seq = (label_seq - self.config['time_calc_s_mean']) / self.config['time_calc_s_std']
         if not self.add_grid_features:
-            return {"samp": samp}
+            return {"samp": samp, "norm_label": norm_label, "norm_label_seq": norm_label_seq}
         else:
             xbin_idxs, ybin_idxs = self.grid.digitize_points(samp[:,self.col_names.index("x")], samp[:,self.col_names.index("y")])
             grid_features = self.grid.get_grid_features(xbin_idxs, ybin_idxs, samp[:,self.col_names.index("locationtime")])
-            return {"samp": samp, "grid": grid_features}
+            return {"samp": samp, "grid": grid_features, "norm_label": norm_label, "norm_label_seq": norm_label_seq}
     def __len__(self):
         return len(self.shingle_keys)
     def get_all_samples(self, keep_cols, indexes=None):
@@ -171,106 +175,106 @@ def basic_collate(batch):
     em_cols = ["timeID","weekID"]
     first_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","passed_stops_n","speed_m_s","bearing"]
     last_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","passed_stops_n","dist_cumulative_km","bearing"]
-    batch = [torch.tensor(b['samp']) for b in batch]
+    y = torch.tensor([b['norm_label'] for b in batch], dtype=torch.float)
+    batch = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch], axis=0)
     last = torch.cat([b[-1,:].unsqueeze(0) for b in batch], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     X_ct_first = first[:,([FEATURE_COLS.index(z) for z in first_cols])]
     X_ct_last = last[:,([FEATURE_COLS.index(z) for z in last_cols])]
     X_ct = torch.cat([X_ct_first, X_ct_last], axis=1)
-    y = last[:,(FEATURE_COLS.index(y_col))]
-    return [X_em.int(), X_ct.float()], y.float()
+    return [X_em, X_ct], y
 def basic_collate_nosch(batch):
     y_col = "time_cumulative_s"
     em_cols = ["timeID","weekID"]
     first_cols = ["x_cent","y_cent","speed_m_s","bearing"]
     last_cols = ["x_cent","y_cent","dist_cumulative_km","bearing"]
-    batch = [torch.tensor(b['samp']) for b in batch]
+    y = torch.tensor([b['norm_label'] for b in batch], dtype=torch.float)
+    batch = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch], axis=0)
     last = torch.cat([b[-1,:].unsqueeze(0) for b in batch], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     X_ct_first = first[:,([FEATURE_COLS.index(z) for z in first_cols])]
     X_ct_last = last[:,([FEATURE_COLS.index(z) for z in last_cols])]
     X_ct = torch.cat([X_ct_first, X_ct_last], axis=1)
-    y = last[:,(FEATURE_COLS.index(y_col))]
-    return [X_em.int(), X_ct.float()], y.float()
+    return [X_em, X_ct], y
 def basic_grid_collate(batch):
     y_col = "time_cumulative_s"
     em_cols = ["timeID","weekID"]
     first_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","passed_stops_n","speed_m_s","bearing"]
     last_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","passed_stops_n","dist_cumulative_km","bearing"]
-    batch_ct = [torch.tensor(b['samp']) for b in batch]
+    y = torch.tensor([b['norm_label'] for b in batch], dtype=torch.float)
+    batch_ct = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch_ct], axis=0)
     last = torch.cat([b[-1,:].unsqueeze(0) for b in batch_ct], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     X_ct_first = first[:,([FEATURE_COLS.index(z) for z in first_cols])]
     X_ct_last = last[:,([FEATURE_COLS.index(z) for z in last_cols])]
     X_ct = torch.cat([X_ct_first, X_ct_last], axis=1)
-    y = last[:,(FEATURE_COLS.index(y_col))]
     # Get speed/bearing/obs age from grid results; average out all values across timesteps; 1 value per cell/obs/variable
-    X_gr = torch.cat([torch.nanmean(torch.tensor(z['grid'])[:,3:,:,:,:], dim=0).unsqueeze(0) for z in batch])
+    X_gr = torch.cat([torch.nanmean(torch.tensor(z['grid'], dtype=torch.float)[:,3:,:,:,:], dim=0).unsqueeze(0) for z in batch])
     # Replace any nans with the average for that feature
     means = torch.nanmean(torch.swapaxes(X_gr, 0, 1).flatten(1), dim=1)
     for i,m in enumerate(means):
         X_gr[:,i,:,:,:] = torch.nan_to_num(X_gr[:,i,:,:,:], m)
-    return [X_em.int(), X_ct.float(), X_gr.float()], y.float()
+    return [X_em, X_ct, X_gr], y
 def basic_grid_collate_nosch(batch):
     y_col = "time_cumulative_s"
     em_cols = ["timeID","weekID"]
     first_cols = ["x_cent","y_cent","speed_m_s","bearing"]
     last_cols = ["x_cent","y_cent","dist_cumulative_km","bearing"]
-    batch_ct = [torch.tensor(b['samp']) for b in batch]
+    y = torch.tensor([b['norm_label'] for b in batch], dtype=torch.float)
+    batch_ct = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch_ct], axis=0)
     last = torch.cat([b[-1,:].unsqueeze(0) for b in batch_ct], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     X_ct_first = first[:,([FEATURE_COLS.index(z) for z in first_cols])]
     X_ct_last = last[:,([FEATURE_COLS.index(z) for z in last_cols])]
     X_ct = torch.cat([X_ct_first, X_ct_last], axis=1)
-    y = last[:,(FEATURE_COLS.index(y_col))]
     # Get speed/bearing/obs age from grid results; average out all values across timesteps; 1 value per cell/obs/variable
-    X_gr = torch.cat([torch.nanmean(torch.tensor(z['grid'])[:,3:,:,:,:], dim=0).unsqueeze(0) for z in batch])
+    X_gr = torch.cat([torch.nanmean(torch.tensor(z['grid'], dtype=torch.float)[:,3:,:,:,:], dim=0).unsqueeze(0) for z in batch])
     # Replace any nans with the average for that feature
     means = torch.nanmean(torch.swapaxes(X_gr, 0, 1).flatten(1), dim=1)
     for i,m in enumerate(means):
         X_gr[:,i,:,:,:] = torch.nan_to_num(X_gr[:,i,:,:,:], m)
-    return [X_em.int(), X_ct.float(), X_gr.float()], y.float()
+    return [X_em, X_ct, X_gr], y
 
 def sequential_collate(batch):
     y_col = "time_calc_s"
     em_cols = ["timeID","weekID"]
     ct_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","stop_x_cent","stop_y_cent","passed_stops_n","bearing","dist_calc_km","dist_calc_km"]
-    batch = [torch.tensor(b['samp']) for b in batch]
-    X_sl = torch.tensor([len(b) for b in batch])
+    y = torch.tensor([b['norm_label_seq'] for b in batch], dtype=torch.float)
+    batch = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
+    X_sl = torch.tensor([len(b) for b in batch], dtype=torch.int)
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True)
     X_ct = batch[:,:,([FEATURE_COLS.index(z) for z in ct_cols])]
-    y = batch[:,:,(FEATURE_COLS.index(y_col))]
-    return [X_em.int(), X_ct.float(), X_sl.int()], y.float()
+    return [X_em, X_ct, X_sl], y
 def sequential_collate_nosch(batch):
     y_col = "time_calc_s"
     em_cols = ["timeID","weekID"]
     ct_cols = ["x_cent","y_cent","bearing","dist_calc_km"]
-    batch = [torch.tensor(b['samp']) for b in batch]
-    X_sl = torch.tensor([len(b) for b in batch])
+    y = torch.tensor([b['norm_label_seq'] for b in batch], dtype=torch.float)
+    batch = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
+    X_sl = torch.tensor([len(b) for b in batch], dtype=torch.int)
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True)
     X_ct = batch[:,:,([FEATURE_COLS.index(z) for z in ct_cols])]
-    y = batch[:,:,(FEATURE_COLS.index(y_col))]
-    return [X_em.int(), X_ct.float(), X_sl.int()], y.float()
+    return [X_em, X_ct, X_sl], y
 def sequential_grid_collate(batch):
     y_col = "time_calc_s"
     em_cols = ["timeID","weekID"]
     ct_cols = ["x_cent","y_cent","scheduled_time_s","stop_dist_km","stop_x_cent","stop_y_cent","passed_stops_n","bearing","dist_calc_km","dist_calc_km"]
-    batch_ct = [torch.tensor(b['samp']) for b in batch]
-    batch_gr = [torch.tensor(b['grid']) for b in batch]
-    X_sl = torch.tensor([len(b) for b in batch_ct])
+    y = torch.tensor([b['norm_label_seq'] for b in batch], dtype=torch.float)
+    batch_ct = [torch.tensor(b['samp'], dtype=torch.float) for b in batch]
+    batch_gr = [torch.tensor(b['grid'], dtype=torch.float) for b in batch]
+    X_sl = torch.tensor([len(b) for b in batch_ct], dtype=torch.int)
     first = torch.cat([b[0,:].unsqueeze(0) for b in batch_ct], axis=0)
-    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])]
+    X_em = first[:,([FEATURE_COLS.index(z) for z in em_cols])].int()
     batch_ct = torch.nn.utils.rnn.pad_sequence(batch_ct, batch_first=True)
     X_ct = batch_ct[:,:,([FEATURE_COLS.index(z) for z in ct_cols])]
-    y = batch_ct[:,:,(FEATURE_COLS.index(y_col))]
     # Get speed/bearing/obs age from grid results; average out all values across timesteps; 1 value per cell/obs/variable
     X_gr = [b[:,3:,:,:,:] for b in batch_gr]
     X_gr = torch.nn.utils.rnn.pad_sequence(X_gr, batch_first=True)
@@ -278,7 +282,7 @@ def sequential_grid_collate(batch):
     means = torch.nanmean(torch.swapaxes(X_gr, 0, 2).flatten(1), dim=1)
     for i,m in enumerate(means):
         X_gr[:,:,i,:,:,:] = torch.nan_to_num(X_gr[:,:,i,:,:,:], m)
-    return [X_em.int(), X_ct.float(), X_gr.float(), X_sl.int()], y.float()
+    return [X_em, X_ct, X_gr, X_sl], y
 
 def deeptte_collate(data):
     stat_attrs = ['dist_cumulative_km', 'time_cumulative_s']