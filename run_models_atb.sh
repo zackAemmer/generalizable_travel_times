e3bda896a00bea930df2ad8073fc6966a1e8e0be
zackAemmer <zae5op@gmail.com>
2023-08-22 20:12:27 -0700

    Big run

--- run_models_atb.sh
+++ run_models_atb.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 set -e
 
-RUN_NAME="full_run"
+RUN_NAME="big_run"
 
 
 cd ~/Desktop/valle
 
 # Run Models
-python ./src/run_one_model.py "FF" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "CONV" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "FF" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "CONV" "./results/$RUN_NAME/" "atb/" False False
 python ./src/run_one_model.py "GRU" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "TRSF" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "DEEP_TTE" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "FF_GTFS" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "CONV_GTFS" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "TRSF" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "DEEP_TTE" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "FF_GTFS" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "CONV_GTFS" "./results/$RUN_NAME/" "atb/" False False
 python ./src/run_one_model.py "GRU_GTFS" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "TRSF_GTFS" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "DEEP_TTE_GTFS" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "FF_GRID" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "CONV_GRID" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "TRSF_GTFS" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "DEEP_TTE_GTFS" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "FF_GRID" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "CONV_GRID" "./results/$RUN_NAME/" "atb/" False False
 python ./src/run_one_model.py "GRU_GRID" "./results/$RUN_NAME/" "atb/" False False
-python ./src/run_one_model.py "TRSF_GRID" "./results/$RUN_NAME/" "atb/" False False
+# python ./src/run_one_model.py "TRSF_GRID" "./results/$RUN_NAME/" "atb/" False False
 
 # Run Experiments
-python ./src/run_experiments.py "FF" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "CONV" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "FF" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "CONV" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
 python ./src/run_experiments.py "GRU" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "TRSF" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "DEEP_TTE" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "FF_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "CONV_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "TRSF" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "DEEP_TTE" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "FF_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "CONV_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
 python ./src/run_experiments.py "GRU_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "TRSF_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
-python ./src/run_experiments.py "DEEP_TTE_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "TRSF_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
+# python ./src/run_experiments.py "DEEP_TTE_GTFS" "./results/$RUN_NAME/" "atb/" "kcm/" "kcm/" False False
